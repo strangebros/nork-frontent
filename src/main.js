d24d38e236f8fe1e4abd1689bee72e7bdc2ebf6f
sootudio <kswim8914@gmail.com>
2024-05-17 15:32:00 +0900

    chore: 아이콘 적용을 위한 fontawsome 설치 및 설정

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import "@/assets/css/reset.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 import App from "./App.vue";
 import router from "./router";
