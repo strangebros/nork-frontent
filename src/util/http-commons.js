3b7d0af46d7c35d795235edf2c98eabf8fc1c660
gmelon <hsh1769@naver.com>
2024-05-20 11:24:49 +0900

    feat: axios util 틀 작성

---
+++ src/util/http-commons.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { httpStatusCode } from "./http-status";
+
+const { BACKEND_URL } = import.meta.env;
+
+// local vue api axios instance
+function norkAxios() {
+  const instance = axios.create({
+    baseURL: BACKEND_URL,
+  });
+
+  // request default settings
+  instance.defaults.headers.common["Authorization"] = "";
+  instance.defaults.headers.post["Content-Type"] = "application/json";
+  instance.defaults.headers.put["Content-Type"] = "application/json";
+
+  // request interceptor
+  instance.interceptors.request.use(
+    (config) => {
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  // response interceptor
+  instance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    async (error) => {
+      const {
+        config,
+        response: { status },
+      } = error;
+
+      return Promise.reject(error);
+    }
+  );
+  return instance;
+}
+
+export { norkAxios };
