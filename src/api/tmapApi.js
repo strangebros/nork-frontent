690da852390b19f61a15a8eceb223e8b6e0fa00c
Sanghyeok Hyun <gmelon24kr@gmail.com>
2024-05-21 14:10:52 +0900

    feat/11-mainPage 메인 페이지 구현 (#12)
    * feat: 메인 페이지 골격 완성
    
    * feat: 메인 페이지 쿼리 검색 기능 완성

---
+++ src/api/tmapApi.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const tmapApi = {
+  async getResultNames(query) {
+    let results = [];
+    await axios
+      .get(
+        `https://apis.openapi.sk.com/tmap/pois?version=1&searchKeyword=${query}`,
+        {
+          headers: {
+            appKey: import.meta.env.VITE_TMAP_KEY,
+          },
+        }
+      )
+      .then((response) => {
+        results = response.data.searchPoiInfo.pois.poi.map((poi) => poi.name);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    return results;
+  },
+};
+
+export default tmapApi;
