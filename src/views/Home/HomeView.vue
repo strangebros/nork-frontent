8d11a02e8afdd04ab93cefcdd32786c44b036142
gmelon <hsh1769@naver.com>
2024-05-21 13:16:26 +0900

    feat: 메인 페이지 골격 완성

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -1,17 +1,338 @@
 <script setup>
 import { useRouter } from "vue-router";
-import { useThemeStore } from '@/stores/theme';
+import { useThemeStore } from "@/stores/theme";
 import { storeToRefs } from "pinia";
+
+// tailwind
+import { ref, computed } from "vue";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxButton,
+  ComboboxOptions,
+  ComboboxOption,
+  TransitionRoot,
+  RadioGroup,
+  RadioGroupLabel,
+  RadioGroupDescription,
+  RadioGroupOption,
+} from "@headlessui/vue";
+import { CheckIcon, ChevronDownIcon } from "@heroicons/vue/20/solid";
 
 const themeStore = useThemeStore();
 const router = useRouter();
-const {darkMode} = storeToRefs(themeStore);
+const { darkMode } = storeToRefs(themeStore);
+
+// searchbar start
+const results = [
+  { id: 1, name: "Wade Cooper" },
+  { id: 2, name: "Arlene Mccoy" },
+  { id: 3, name: "Devon Webb" },
+  { id: 4, name: "Tom Cook" },
+  { id: 5, name: "Tanya Fox" },
+  { id: 6, name: "Hellen Schmidt" },
+];
+
+let selectedQuery = ref(-1);
+let query = ref("");
+
+let filteredPeople = computed(() =>
+  query.value === ""
+    ? results
+    : results.filter((person) =>
+        person.name
+          .toLowerCase()
+          .replace(/\s+/g, "")
+          .includes(query.value.toLowerCase().replace(/\s+/g, ""))
+      )
+);
+// searchbar end
+
+// options start
+const distanceOptions = [
+  {
+    radius: "-1",
+    label: "어디든 갈 수 있어요",
+  },
+  {
+    radius: "2",
+    label: "우리 동네였으면 좋겠어요",
+  },
+  {
+    radius: "1",
+    label: "집 앞도 가기 힘들어요",
+  },
+];
+
+const keywordOptions = [
+  {
+    radius: "0",
+    label: "조용한",
+  },
+  {
+    radius: "1",
+    label: "시끄러운",
+  },
+  {
+    radius: "2",
+    label: "신나는",
+  },
+];
+
+const selectedDistance = ref(distanceOptions[0]);
+const selectedKeyword = ref(keywordOptions[0]);
+// options end
 </script>
 
 <template>
-  <div :class="{ dark: darkMode }">
-    <h1>Main 페이지 입니다.</h1>
-    <RouterLink to="/map"> map으로 이동 </RouterLink>
+  <div class="flex justify-center" :class="{ dark: darkMode }">
+    <div class="flex flex-col w-5/6 sm:w-2/3 xl:w-1/2 justify-center">
+      <div
+        id="recommendWrapper"
+        class="flex justify-center items-center text-sm bg-gray-200 dark:bg-gray-700 rounded-lg mb-5 p-2 dark:text-white"
+      >
+        <p class="mr-3">
+          gmelon님, 최근 <span class="font-semibold">'개발자'</span> 직군에서
+          핫한 장소들 어떠신가요?
+        </p>
+        <button class="bg-primary-light text-white px-3 py-1.5 rounded-lg">
+          추천받기
+        </button>
+      </div>
+      <div
+        id="searchWrapper"
+        class="flex items-center flex-col rounded-lg p-4 bg-gray-200 dark:bg-gray-700 dark:text-white"
+      >
+        <div id="searchBar" class="flex items-center mb-4">
+          <Combobox v-model="selectedQuery" class="mr-3">
+            <div class="relative sm:w-80 md:w-96">
+              <div
+                class="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm"
+              >
+                <ComboboxInput
+                  class="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+                  :displayValue="
+                    (index) => {
+                      if (index >= 0) {
+                        return results[index].name;
+                      }
+                      return '어떤 장소를 찾으시나요?';
+                    }
+                  "
+                  @change="query = $event.target.value"
+                />
+                <ComboboxButton
+                  class="absolute inset-y-0 right-0 flex items-center pr-2"
+                >
+                  <ChevronDownIcon
+                    class="h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                </ComboboxButton>
+              </div>
+              <TransitionRoot
+                leave="transition ease-in duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+                @after-leave="query = ''"
+              >
+                <ComboboxOptions
+                  class="absolute z-50 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm"
+                >
+                  <div
+                    v-if="filteredPeople.length === 0 && query !== ''"
+                    class="relative cursor-default select-none px-4 py-2 text-gray-700"
+                  >
+                    Nothing found.
+                  </div>
+
+                  <ComboboxOption
+                    v-for="person in filteredPeople"
+                    as="template"
+                    :key="person.id"
+                    :value="person"
+                    v-slot="{ selectedQuery, active }"
+                  >
+                    <li
+                      class="relative cursor-default select-none py-2 pl-10 pr-4"
+                      :class="{
+                        'bg-primary-light text-white': active,
+                        'text-gray-900': !active,
+                      }"
+                    >
+                      <span
+                        class="block truncate"
+                        :class="{
+                          'font-medium': selectedQuery,
+                          'font-normal': !selectedQuery,
+                        }"
+                      >
+                        {{ person.name }}
+                      </span>
+                      <span
+                        v-if="selectedQuery"
+                        class="absolute inset-y-0 left-0 flex items-center pl-3"
+                        :class="{
+                          'text-white': active,
+                          'text-primary-light': !active,
+                        }"
+                      >
+                        <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                      </span>
+                    </li>
+                  </ComboboxOption>
+                </ComboboxOptions>
+              </TransitionRoot>
+            </div>
+          </Combobox>
+          <button class="bg-primary-light text-white px-3 py-1.5 rounded-lg">
+            검색
+          </button>
+        </div>
+        <div id="searchOptions" class="flex justify-around">
+          <div id="distanceOptions" class="w-48 max-w-md text-sm mr-3">
+            <RadioGroup v-model="selectedDistance">
+              <RadioGroupLabel class="ml-1 font-semibold"
+                >현재 나의 체력은?</RadioGroupLabel
+              >
+              <div class="space-y-2 mt-2">
+                <RadioGroupOption
+                  as="template"
+                  v-for="option in distanceOptions"
+                  :key="option.radius"
+                  :value="option"
+                  v-slot="{ active, checked }"
+                >
+                  <div
+                    :class="[
+                      active
+                        ? 'ring-2 ring-white/60 ring-offset-2 ring-offset-sky-300'
+                        : '',
+                      checked ? 'bg-primary-dark text-white ' : 'bg-white ',
+                    ]"
+                    class="relative flex cursor-pointer rounded-3xl px-4 py-3 shadow-md focus:outline-none"
+                  >
+                    <div class="flex w-full items-center justify-between">
+                      <div class="flex items-center">
+                        <div class="text-sm">
+                          <RadioGroupLabel
+                            as="p"
+                            :class="checked ? 'text-white' : 'text-gray-900'"
+                            class="font-medium text-xs"
+                          >
+                            {{ option.label }}
+                          </RadioGroupLabel>
+                          <RadioGroupDescription
+                            as="span"
+                            :class="checked ? 'text-sky-100' : 'text-gray-500'"
+                            class="inline"
+                          >
+                          </RadioGroupDescription>
+                        </div>
+                      </div>
+                      <div v-show="checked" class="shrink-0 text-white">
+                        <svg class="h-4 w-4" viewBox="0 0 24 24" fill="none">
+                          <circle
+                            cx="12"
+                            cy="12"
+                            r="12"
+                            fill="#fff"
+                            fill-opacity="0.2"
+                          />
+                          <path
+                            d="M7 13l3 3 7-7"
+                            stroke="#fff"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
+                </RadioGroupOption>
+              </div>
+            </RadioGroup>
+          </div>
+          <div
+            style="border-width: 0.5px"
+            class="border-gray-300 h-full mx-4"
+          ></div>
+          <div id="keywordeOptions" class="max-w-md text-sm">
+            <RadioGroup v-model="selectedKeyword">
+              <RadioGroupLabel class="ml-1 font-semibold"
+                >어떤 장소에 가고 싶나요?</RadioGroupLabel
+              >
+              <div class="space-y-2 w-24 mt-2">
+                <RadioGroupOption
+                  as="template"
+                  v-for="option in keywordOptions"
+                  :key="option.radius"
+                  :value="option"
+                  v-slot="{ active, checked }"
+                >
+                  <div
+                    :class="[
+                      active
+                        ? 'ring-2 ring-white/60 ring-offset-2 ring-offset-sky-300'
+                        : '',
+                      checked ? 'bg-primary-dark text-white ' : 'bg-white ',
+                    ]"
+                    class="relative flex cursor-pointer rounded-3xl px-4 py-3 shadow-md focus:outline-none"
+                  >
+                    <div class="flex w-full items-center justify-between">
+                      <div class="flex items-center">
+                        <div class="text-sm">
+                          <RadioGroupLabel
+                            as="p"
+                            :class="checked ? 'text-white' : 'text-gray-900'"
+                            class="font-medium text-xs"
+                          >
+                            {{ option.label }}
+                          </RadioGroupLabel>
+                          <RadioGroupDescription
+                            as="span"
+                            :class="checked ? 'text-sky-100' : 'text-gray-500'"
+                            class="inline"
+                          >
+                          </RadioGroupDescription>
+                        </div>
+                      </div>
+                      <div v-show="checked" class="shrink-0 text-white">
+                        <svg class="h-4 w-4" viewBox="0 0 24 24" fill="none">
+                          <circle
+                            cx="12"
+                            cy="12"
+                            r="12"
+                            fill="#fff"
+                            fill-opacity="0.2"
+                          />
+                          <path
+                            d="M7 13l3 3 7-7"
+                            stroke="#fff"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
+                </RadioGroupOption>
+              </div>
+            </RadioGroup>
+          </div>
+        </div>
+        <p class="text-xs mt-4">
+          아직 결정하지 못했다면,
+          <RouterLink
+            class="text-blue-700 dark:text-blue-300"
+            to="{name: 'myPage'}"
+            >내 지난 기록에서 찾아보세요.</RouterLink
+          >
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
