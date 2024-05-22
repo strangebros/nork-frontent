6e6c8a0a9548a9a0b77cf22e4609302ad668a498
Sanghyeok Hyun <gmelon24kr@gmail.com>
2024-05-22 11:44:13 +0900

    feat: 상세 조회 고도화 및 workspaces 이동 로직 구현 (#17)

--- src/views/Map/MapView.vue
+++ src/views/Map/MapView.vue
@@ -108,7 +108,9 @@ function calculateRadius() {
 }
 
 let workspaces = ref([]);
-const searchWorkspaces = async () => {
+let nowLoading = ref(false);
+const searchWorkspaces = async (move) => {
+  nowLoading.value = true;
   await workspaceApi.searchAll(
     {
       query: selectedQuery.value,
@@ -126,12 +128,13 @@ const searchWorkspaces = async () => {
         workspaces.value = [];
         return;
       }
-      refreshBoundsAndMarkers(workspaces.value);
+      refreshBoundsAndMarkers(workspaces.value, move);
     },
     (error) => {
       console.log(error);
     }
   );
+  nowLoading.value = false;
 };
 
 const searchWorkspaceOne = async (poiId) => {
@@ -168,7 +171,7 @@ const onLoadKakaoMap = (mapRef) => {
   currentBounds.value = new kakao.maps.LatLngBounds();
 };
 // map options end
-const refreshBoundsAndMarkers = (workspaces) => {
+const refreshBoundsAndMarkers = (workspaces, move) => {
   currentMarkers.value = workspaces.map((workspace) => {
     // bound
     let bound = new kakao.maps.LatLng(workspace.latitude, workspace.longitude);
@@ -188,14 +191,26 @@ const refreshBoundsAndMarkers = (workspaces) => {
     return marker;
   });
 
-  map.value.setBounds(currentBounds.value);
+  if (move) {
+    map.value.setBounds(currentBounds.value);
+  }
 };
 
 const markerMouseOut = (marker) => {
   marker.visible = false;
 };
 
-let jobs = ["개발자", "디자이너", "기획자"];
+let jobs = [
+  `<p class="mr-1 px-2 py-1 bg-primary-light rounded-3xl text-white">
+          개발자
+        </p>`,
+  `<p class="mr-1 px-2 py-1 bg-yellow-500 rounded-3xl text-white">
+          디자이너
+        </p>`,
+  `<p class="mr-1 px-2 py-1 bg-green-500 rounded-3xl text-white">
+          기획자
+        </p>`,
+];
 
 const markerMouseOver = async (marker) => {
   currentMarkers.value.forEach((marker) => (marker.visible = false));
@@ -207,10 +222,7 @@ const markerMouseOver = async (marker) => {
 
   let currentWorkersHtml = "";
   for (let index = 0; index < workerCount; index++) {
-    currentWorkersHtml += `
-        <p class="mr-1 px-2 py-1 bg-primary-light rounded-3xl text-white">
-          ${jobs[Math.ceil(Math.random() * (jobs.length - 1))]}
-        </p>
+    currentWorkersHtml += `${jobs[Math.ceil(Math.random() * (jobs.length - 1))]}
       `;
   }
 
@@ -219,7 +231,6 @@ const markerMouseOver = async (marker) => {
   for (let index = 0; index < result.imageUrls.length; index++) {
     let img = new Image();
     img.onload = function () {
-      console.log("로드됨");
       images.value.push(result.imageUrls[index]);
     };
     img.src = result.imageUrls[index];
@@ -274,6 +285,13 @@ const markerMouseOver = async (marker) => {
             조용한
           </p>
         </div>
+        <hr class="mt-3">
+        <a href="./workspaces/${
+          result.poiId
+        }"><div class="px-2 mt-3 text-white rounded-full py-1 text-sm font-semibold text-center bg-primary-light">
+        이 장소를 사용할래요!
+        </div>
+        </a>
       </div>
   `;
 
@@ -284,6 +302,14 @@ const markerMouseOver = async (marker) => {
       "markerInfo"
     ).parentNode.parentNode.style.borderRadius = "10px";
   });
+};
+
+const clickSearchBarWorkspace = (workspace) => {
+  markerMouseOver(workspace.marker);
+
+  map.value.panTo(
+    new kakao.maps.LatLng(workspace.marker.lat, workspace.marker.lng)
+  );
 };
 </script>
 
@@ -409,10 +435,37 @@ const markerMouseOver = async (marker) => {
         <div class="flex w-full">
           <p class="ml-6 mt-3 font-bold text-lg">검색 결과</p>
         </div>
+        <div v-if="nowLoading" class="text-center my-3">
+          <div role="status">
+            <svg
+              aria-hidden="true"
+              class="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+            <span class="sr-only">Loading...</span>
+          </div>
+        </div>
         <template v-for="workspace in workspaces" :key="workspace.id">
           <div class="searchResult flex flex-col items-start w-full px-6 py-3">
             <div class="flex items-end">
-              <p class="text-primary-light font-bold">{{ workspace.name }}</p>
+              <p
+                style="cursor: pointer"
+                class="text-primary-light font-bold"
+                @click="clickSearchBarWorkspace(workspace)"
+              >
+                {{ workspace.name }}
+              </p>
               <p class="ml-2 text-xs text-text">{{ workspace.category }}</p>
             </div>
             <p class="text-xs font-normal text-text mt-2">
@@ -450,7 +503,13 @@ const markerMouseOver = async (marker) => {
         <
       </div>
     </div>
-    <div style="width: calc(100vw - 350px)" class="map">
+    <div style="width: calc(100vw - 350px)" class="map flex justify-center">
+      <button
+        @click="searchWorkspaces(false)"
+        class="absolute z-50 text-xs px-4 py-2 opacity-90 mt-3 bg-primary-light rounded-full text-white"
+      >
+        현 위치에서 재검색
+      </button>
       <KakaoMap
         :lat="coordinate.lat"
         :lng="coordinate.lng"
